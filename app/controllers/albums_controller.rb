9da986825a6fdde152cc397b24ce8eaaf370bb73
Nidhi Mehta <nidhididi@gmail.com>
2014-03-11 10:46:41 -0400

    added items to navbar

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -69,6 +69,6 @@ class AlbumsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def album_params
-      params.require(:album).permit(:name, :num_songs, :release_date)
+      params.require(:album).permit(:name, :num_songs, :release_date, :photo_url)
     end
 end
